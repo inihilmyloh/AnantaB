61164e26205720ccb64656500776fecee5a6cae9
choos <choos@LAPTOP-JKHKBRBM>
2025-02-24 08:56:06 +0700

    Sudah selesai gaes

--- src/login/logis.java
+++ src/login/logis.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package login;
 
 /**
  *
  * @author hilmy
  */
-public class logis {
-    public static void main(String[] args) {
-        System.out.println("shfsj");
+
     }
     
 }
